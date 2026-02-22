54cc5842b103317c456851b999b96a1c0ba54caa
RashidVelery <rashivelery@gamil.com>
2026-02-22 17:50:38 +0530

    Updated project

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -14,7 +14,7 @@ connectDB()
 app.use(express.json());
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://ptm-projectfrontend.vercel.app/",
         credentials: true,
     })
 );
